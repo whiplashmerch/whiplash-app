bffea898d7ca05022b1cf33b422cea5a402441af
Don Sullivan <apologeticdon@gmail.com>
2016-08-10 12:42:28 -0400

    adding tests.

--- spec/whiplash/app_spec.rb
+++ spec/whiplash/app_spec.rb
@@ -1,20 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Whiplash::App do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Whiplash::App::VERSION).not_to be nil
   end
 
-  describe '#client' do
-    it 'has an oauth client' do
+  describe "#client" do
+    it "has an oauth client" do
       expect(Whiplash::App.client).to be_a OAuth2::Client
     end
   end
 
-  describe '#connection' do
-    it 'has a faraday connection' do
+  describe "#connection" do
+    it "has a faraday connection" do
       Whiplash::App.stub(:token).and_return("bacon")
       expect(Whiplash::App.connection).to be_a Faraday::Connection
+    end
+  end
+
+  describe "#get" do
+    it "returns a specified resource" do
+      orders = Whiplash::App.get('orders', {}, {})
+      expect(orders).to eq("blah")
     end
   end
 
