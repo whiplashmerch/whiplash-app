cfc4826d78078d7d1cd741d084c6bc5c1a6d4e92
ideaoforder <mark@sitesteaders.com>
2017-08-30 11:48:48 -0400

    allow passing in token; better refresh; check expired token; set customer and shop attributes for headers

--- lib/whiplash/app.rb
+++ lib/whiplash/app.rb
@@ -17,13 +17,14 @@ module Whiplash
       include Whiplash::App::FinderMethods
       include Whiplash::App::Signing
 
-      attr_accessor :customer_id, :shop_id
+      attr_accessor :customer_id, :shop_id, :token
 
       def client
         OAuth2::Client.new(ENV["WHIPLASH_CLIENT_ID"], ENV["WHIPLASH_CLIENT_SECRET"], site: api_url)
       end
 
       def connection
+        token = refresh_token! if token_expired?
         out = Faraday.new [api_url, "api/v2"].join("/") do |conn|
           conn.request :oauth2, token, token_type: "bearer"
           conn.request :json
@@ -34,16 +35,29 @@ module Whiplash
         return out
       end
 
-      def refresh_token!
-        oauth_token = client.client_credentials.get_token(scope: ENV["WHIPLASH_CLIENT_SCOPE"])
-        new_token = oauth_token.token
-        cache_store["whiplash_api_token"] = new_token
+      def token=(oauth_token)
+        oauth_token = OAuth2::AccessToken.from_hash(client, oauth_token) unless oauth_token.is_a?(OAuth2::AccessToken)
+        super(oauth_token)
       end
 
-      def token
-        refresh_token! unless cache_store["whiplash_api_token"]
-        return cache_store["whiplash_api_token"]
+      def refresh_token!
+        if token.blank? # If we're storing locally, grab a new token and cache it
+          access_token = client.client_credentials.get_token(scope: ENV["WHIPLASH_CLIENT_SCOPE"])
+          new_token = access_token.to_hash
+          cache_store["whiplash_api_token"] = new_token
+          access_token
+        else
+          token.refresh!
+        end
       end
+
+      def token_expired?
+        return token.expired? unless token.blank?
+        return true unless cache_store.has_key?("whiplash_api_token")
+        return true if cache_store["whiplash_api_token"].blank?
+        false
+      end
+
     end
   end
 end
