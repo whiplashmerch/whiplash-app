2fdfeb5cb6e0ab6645bb6cf30412ff71c22a3f8d
rs <ryanselk@gmail.com>
2021-10-18 10:08:09 -0600

    Fix spec and bump version

--- spec/whiplash/app_specs/connections_spec.rb
+++ spec/whiplash/app_specs/connections_spec.rb
@@ -7,8 +7,8 @@ describe Whiplash::App::Connections do
 
   before do
     allow_any_instance_of(Whiplash::App).to receive(:token).and_return(double(token: 'access token'))
-    allow_any_instance_of(Faraday::Connection).to receive(:send).with(:get, "core_url", {:per_page=>50}, {}).and_return(first_request)
-    allow_any_instance_of(Faraday::Connection).to receive(:send).with(:get, "core_url", {:page => 2, :per_page=>50}, {}).and_return(second_request)
+    allow_any_instance_of(Faraday::Connection).to receive(:send).with(:get, "core_url", {:per_page=>50}, {"Accept-Version"=>"v2"}).and_return(first_request)
+    allow_any_instance_of(Faraday::Connection).to receive(:send).with(:get, "core_url", {:page => 2, :per_page=>50}, {"Accept-Version" => "v2"}).and_return(second_request)
   end
 
   context 'GET calls' do
