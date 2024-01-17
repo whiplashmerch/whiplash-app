8fc475489c7932736e338c9c8c1ed1e67d684e04
ideaoforder <mark@sitesteaders.com>
2024-01-16 20:24:18 -0500

    application_url not app_url; make sure application_url is defined

--- lib/whiplash/app/canonical_host.rb
+++ lib/whiplash/app/canonical_host.rb
@@ -9,7 +9,8 @@ module Whiplash
       def require_canonical_host!
         canonical_host = ENV.fetch('CANONICAL_HOST', false).in?([true, 'true', 1, '1'])
         return unless canonical_host
-        application_host = URI.parse(Rails.configuration.app_url).host
+        return unless Rails.configuration.respond_to?(:application_url)
+        application_host = URI.parse(Rails.configuration.application_url).host
         return if application_host == request.host
         return unless request.method_symbol == :get # can't redirect PUT, POST, DELETE
     
@@ -17,7 +18,7 @@ module Whiplash
       end
     
       def redirect_to_canonical_host(query_params, status=301)
-        redirect_to "#{Rails.configuration.app_url}#{request.path}#{'?' if query_params.to_query.present?}#{query_params.to_query}", status: status
+        redirect_to "#{Rails.configuration.application_url}#{request.path}#{'?' if query_params.to_query.present?}#{query_params.to_query}", status: status
       end
 
     end
